248c676ebf16d0feaf26d9ccb683d9e1ebec810c
rayme persad <raymepersad@gmail.com>
2021-06-06 23:49:20 -0400

    Initial commit

---
+++ src/main/java/org/example/practiceexample22.java
@@ -0,0 +1,27 @@
+/*
+ *   UCF COP3330 SUMMER 2021 ASSIGNMENT 12 SOLUTION
+ *   COPYRIGHT 2021 RAYME PERSAD
+ */
+
+/*Write a program that asks for three numbers.
+Check first to see that all numbers are different. If they’re not different, then exit the program.
+Otherwise, display the largest number of the three.
+ */
+
+
+package org.example;
+import java.util.Scanner;
+
+public class practiceexample22 {
+        public static void main(String[] args)
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input the first number : "); //prompt first number input
+            int x = in.nextInt();
+            System.out.print("Input the second number: "); //prompt second number input
+            int y = in.nextInt();
+            System.out.print("Input the third number : "); //prompt third number input
+            int z = in.nextInt();
+           
+    }
+}
